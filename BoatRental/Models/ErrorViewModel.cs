c47e71702a54230c2456b2f04e10718514402f1d
Shalabh Mishra <shalabhm24@hotmail.com>
2020-09-15 22:21:29 +0530

    Add project files.

---
+++ BoatRental/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BoatRental.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
